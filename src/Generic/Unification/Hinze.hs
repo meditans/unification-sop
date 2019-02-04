d37fd971ec9388709f83f1fbf4a9f2719b0c1d33
Carlo Nucera <meditans@gmail.com>
2019-02-04 01:51:46 +0100

    cleanup the prolog example in Hinze
    this version has the basic features I would expect. What should be done
    next are attributed variables to implement dif/2.

--- src/Generic/Unification/Hinze.hs
+++ src/Generic/Unification/Hinze.hs
@@ -2,68 +2,90 @@
 -- Hinze, because I only want a clear denotational semantic for now. This is
 -- done as an experiment, and will not be part of the final package.
 
-{-# LANGUAGE ExistentialQuantification, RankNTypes, FlexibleInstances, UndecidableInstances, FlexibleContexts, DeriveGeneric, GeneralizedNewtypeDeriving, MultiParamTypeClasses, TupleSections, DerivingStrategies, DerivingVia #-}
+{-# LANGUAGE DeriveGeneric, DerivingStrategies, DerivingVia   #-}
+{-# LANGUAGE ExistentialQuantification, FlexibleContexts      #-}
+{-# LANGUAGE FlexibleInstances, GeneralizedNewtypeDeriving    #-}
+{-# LANGUAGE MultiParamTypeClasses, RankNTypes, TupleSections #-}
+{-# LANGUAGE UndecidableInstances                             #-}
 
-module Generic.Unification.Hinze where
+module Generic.Unification.Hinze
+  ( Backtr (..)
+  , Cut (..)
+  , naf, naf2, only
+  , BacktrT (..)
+  , CutT (..)
+  , Logic (..)
+  , runLogic, evalLogic, (===)
+  ) where
 
-import Prelude hiding (fail)
-import Control.Monad hiding (fail)
-import Control.Monad.Identity hiding (fail)
-import qualified GHC.Generics as GHC
-import Generics.SOP
-import Control.Monad.Except hiding (fail)
-import Control.Monad.Trans
-import Control.Monad.State hiding (fail)
+import Control.Monad.Except
+import Control.Monad.Identity
+import Control.Monad.State
 import Data.Either (rights)
-import Data.Maybe (listToMaybe)
+import Data.Maybe  (listToMaybe)
+import Generics.SOP
+import qualified GHC.Generics as GHC
 
 import Generic.Unification.Term
 import Generic.Unification.Unification
 import Generic.Unification.Substitution
 
+--------------------------------------------------------------------------------
+-- Implementation, following Hinze
+--------------------------------------------------------------------------------
+
 -- | Important classes
 
 class (Monad m) => Backtr m where
-  fail :: m a
+  -- | No solutions, corresponds to fail in prolog
+  flop :: m a
+  -- | Non-deterministing choice between two branches
   amb  :: m a -> m a -> m a
+  -- | Encapsulation, find one solution
   once :: m a -> m (Maybe a)
+  -- | Encapsulation, find all solutions
   sols :: m a -> m [a]
 
-true :: Monad m => m ()
-true = pure ()
+class (Backtr m) => Cut m where
+  -- | like in prolog, cut discards choicepoints
+  cut  :: m ()
+  -- | to delimit the effect of a cut in a region
+  call :: m a -> m a
+
+--------------------------------------------------------------------------------
+-- Interesting operations with the classes
+--------------------------------------------------------------------------------
 
 -- | Negation as failure
-naf :: (Backtr m) => m a -> m ()
-naf m = once m >>= maybe true (\_ -> fail)
+naf :: Backtr m => m a -> m ()
+naf m = once m >>= maybe true (\_ -> flop)
 
-class (Backtr m) => Cut m where
-  cut  :: m ()
-  call :: m a -> m a -- to delimit the effect of a cut
+-- | Subtly different negation as failure
+naf2 :: Cut m => m a -> m ()
+naf2 m = call ((m >> cut >> flop) `amb` true)
 
+-- | Equivalent to once/1 in prolog. Note, the once function in the typeclass is
+-- the encapsulated version of this operation.
 only :: (Cut m) => a -> m a
 only a = cut >> pure a
 
-naf2 :: Cut m => m a -> m ()  -- subtly different from naf
-naf2 m = call ((m >> cut >> fail) `amb` true) -- cut/fail
-
-child :: (Backtr m) => String -> m String
-child "terach"  = pure "abraham" `amb` pure "nachor" `amb` pure "haran"
-child "abraham" = pure "isaac"
-child "haran"   = pure "lot" `amb` pure "milcah" `amb` pure "yiscah"
-child "sarah"   = pure "isaac"
-child _         = fail
-
-grandChild :: (Backtr m) => String -> m String
-grandChild = child >=> child
+--------------------------------------------------------------------------------
+-- Auxiliary definitions
+--------------------------------------------------------------------------------
 
 class MonadT t where
   up   :: (Monad m) => m a -> t m a
   down :: (Monad m) => t m a -> m a
 
 type CPS a ans = (a -> ans) -> ans
-newtype BacktrT m a = BacktrT { (>>-) :: forall ans. CPS a (m ans -> m ans) }
 
--- >>- :: BacktrT m a -> (a -> ans) -> ans
+data Answer m a = No | Yes a (m (Answer m a))
+
+--------------------------------------------------------------------------------
+-- The BacktrT monad
+--------------------------------------------------------------------------------
+
+newtype BacktrT m a = BacktrT { (>>-) :: forall ans. CPS a (m ans -> m ans) }
 
 instance Functor (BacktrT m) where
   fmap f g = BacktrT $ \h ->
@@ -88,36 +110,14 @@ instance MonadT BacktrT where
   down (BacktrT m) = m (\a _ -> pure a) (error "no solution")
 
 instance (Monad m) => Backtr (BacktrT m) where
-  fail      = BacktrT $ \k -> id
+  flop      = BacktrT $ \_ -> id
   (BacktrT m) `amb` (BacktrT n) = BacktrT $ \k -> m k . n k
-  once (BacktrT m) = up (m first nothing)
+  once (BacktrT m) = up (m just nothing)
   sols (BacktrT m) = up (m cons nil)
 
-nothing :: (Monad m) => m (Maybe a)
-nothing = pure Nothing
-
-nil :: (Monad m) => m [a]
-nil = pure []
-
-cons :: (Monad m) => a -> m [a] -> m [a]
-cons a ms = do
-  as <- ms
-  pure (a : as)
-
-first :: (Monad m) => a -> m (Maybe a) -> m (Maybe a)
-first a _ = pure (Just a)
-
-data Answer m a = No
-                | Yes a (m (Answer m a))
-
-answer :: (Monad m) => m b -> (a -> m b -> m b) -> Answer m a -> m b
-answer no yes No = no
-answer no yes (Yes a m) = yes a (m >>= answer no yes)
-
-no :: (Monad m) => m (Answer m a)
-no = pure No
-yes :: (Monad m) => a -> m (Answer m a) -> m (Answer m a)
-yes a mA = pure (Yes a mA)
+--------------------------------------------------------------------------------
+-- The CutT monad
+--------------------------------------------------------------------------------
 
 newtype CutT m a = CutT { (>>--) :: forall ans. CPS a (m (Answer m ans) -> m (Answer m ans)) }
 
@@ -144,70 +144,60 @@ instance MonadT CutT where
   down (CutT m) = m yes no >>= answer (error "no solution!") (\a _ -> pure a)
 
 instance (Monad m) => Backtr (CutT m) where
-  fail = CutT $ \k -> id
+  flop = CutT $ \_ -> id
   (CutT m) `amb` (CutT n) = CutT $ \k -> m k . n k
-  once (CutT m) = up (m yes no >>= answer nothing first)
+  once (CutT m) = up (m yes no >>= answer nothing just)
   sols (CutT m) = up (m yes no >>= answer nil cons)
 
 instance (Monad m) => Cut (CutT m) where
-  cut = CutT $ \k f -> k () no
+  cut = CutT $ \k _ -> k () no
   call (CutT m) = CutT $ \k f -> m yes no >>= answer f k
 
-class (Monad m) => Run m where
-  run :: m a -> a
+--------------------------------------------------------------------------------
+-- Lifting of Backtr and Cut instances over ExceptT and StateT
+--------------------------------------------------------------------------------
 
-instance (MonadT t, Run m, Monad (t m)) => Run (t m) where
-  run = run . down
+instance Backtr m => Backtr (ExceptT e m) where
+  flop = ExceptT flop
+  (ExceptT t1) `amb` (ExceptT t2) = ExceptT $ t1 `amb` t2
+  once (ExceptT t) = ExceptT (fmap (Right . listToMaybe . rights) $ sols t)
+  sols (ExceptT t) = ExceptT (fmap (Right .               rights) $ sols t)
 
-instance Run Identity where
-  run (Identity a) = a
+instance Backtr m => Backtr (StateT s m) where
+  flop = StateT (\_ -> flop)
+  (StateT a) `amb` (StateT b) = StateT $ \s -> a s `amb` b s
+  once (StateT a) = StateT $ \s -> fmap ((,s) . fmap fst) . once $ a s
+  sols (StateT t) = StateT $ \s -> fmap ((,s) . fmap fst) . sols $ t s
 
-runB :: BacktrT Identity a -> a
-runB = run
+instance Cut m => Cut (ExceptT e m) where
+  cut  = ExceptT $ fmap Right cut
+  call (ExceptT t) = ExceptT (call t)
 
--- >>> run $ sols (child "terach" :: BacktrT Identity String)
--- [ "abraham" , "nachor" , "haran" ]
--- >>> run $ sols (grandChild "terach" :: BacktrT Identity String)
--- [ "isaac" , "lot" , "milcah" , "yiscah" ]
--- >>> take 2 . run $ sols (grandChild "terach" :: BacktrT Identity String)
--- [ "isaac" , "lot" ]
--- >>> run $ (child "isaac" :: BacktrT Identity String)
--- *** Exception: no solution
--- CallStack (from HasCallStack):
---   error, called at /home/carlo/code/haskell/unification-sop/src/Generic/Unification/Hinze.hs:77:42 in unification-sop-0.1.0.0-inplace:Generic.Unification.Hinze
--- >>> run $ once (child "isaac" :: BacktrT Identity String)
--- Nothing
+instance Cut m => Cut (StateT s m) where
+  cut = StateT $ \s -> fmap (,s) cut
+  call (StateT t) = StateT $ \s -> call (t s)
 
--- I could wrap a state monad transformer inside to carry the substitutions and
--- the counter for the fresh variables, and then cook up a way to talk about
--- attributed variables, which is not too expensive computationally.
---
+--------------------------------------------------------------------------------
+-- Logic, directly using UnificationT
+--------------------------------------------------------------------------------
 
--- How would member be implemented here? The point is that without unification
--- member is pointless, because I could just use a haskell function. But, when I
--- have unification, stating member is a constraint that has to be satisfied,
--- and so composes in greater logical programs.
+-- this is morally s -> m [Either e (a, s)]
+newtype Logic a = Logic { unLogic :: UnificationT (CutT Identity) a }
+  deriving (Functor, Applicative, Monad, Backtr, Cut, MonadState Substitution, MonadError UnificationError)
+    via StateT Substitution (ExceptT UnificationError (CutT Identity))
 
--- This lifts unification on terms to a monadic action in backtrackable monads
--- unifyB :: (Unifiable (Term a), Backtr m) => Term a -> Term a -> m (Term a)
--- unifyB t1 t2 = either (const fail) pure $ unify t1 t2
+runLogic :: Logic a -> [Either UnificationError (a, Substitution)]
+runLogic l = runIdentity . down . sols . runUnificationT $ unLogic l
 
--- ("a", X) `member` [("a", "b"), ("b", "c")]
--- memb :: (Unifiable (Term a), Backtr m) => Term a -> [Term a] -> m (Term a)
--- memb a []     = fail
--- memb a (b:bs) = unifyB a b `amb` memb a bs
+evalLogic :: Logic a -> [a]
+evalLogic l = runIdentity . fmap (Prelude.map fst . rights) . down . sols . runUnificationT $ unLogic l
 
--- memb2 :: (Unifiable (Term a)) => Term a -> [Term a] -> Logic (Term a)
--- memb2 a []     = fail
--- memb2 a (b:bs) = a === b `amb` memb2 a bs
+(===) :: Unifiable a => a -> a -> Logic a
+a === b = Logic (unifyVal a b)
 
--- memb3 :: (Unifiable (Term a)) => Term a -> [Term a] -> Logic (Term a, Substitution)
--- memb3 a []     = fail
--- memb3 a (b:bs) = (do n <- a === b; s <- get; pure (n, s) ) `amb` memb3 a bs
-
--- (===) :: Unifiable a => a -> a -> Logic a
--- a === b = Logic $ lift (unifyVal a b)
-
+--------------------------------------------------------------------------------
+-- Member, logical style
+--------------------------------------------------------------------------------
 
 data Pair a = Pair a a deriving (Show, Eq, GHC.Generic)
 instance Generic (Pair a) where
@@ -226,131 +216,79 @@ dict =
   , pair (Con 1) (Con 4)
   ]
 
--- >>> runB $ do a <- sols $ memb key dict ; return (a, key)
--- ( [ pair (Con 1) (Con 2) , pair (Con 1) (Con 4) ]
--- , pair (Con 1) (Var 1)
--- )
---
-
--- But this ^ is not what I have in mind, because it resets the substitutions
--- every time. Instead I want the substitutions to be maintained.
---
-
---------------------------------------------------------------------------------
--- Modern style
---------------------------------------------------------------------------------
-
--- instance MonadTrans CutT where
---   lift m = CutT $ \k f -> m >>= \a -> k a f
-
--- instance MonadState s m => MonadState s (CutT m) where
---   get   = lift get
---   put   = lift . put
---   state = lift . state
-
--- newtype Logic a = Logic { unLogic :: CutT Unification a }
---   deriving (Functor, Applicative, Monad, Backtr, Cut, MonadState Substitution)
-
--- evalLogic :: Logic a -> [a]
--- evalLogic = either (const []) id . evalUnification . down . sols . unLogic
-
--- -- runLogic :: Logic a -> _
--- runLogic :: Logic a -> Either UnificationError ([a], Substitution)
--- runLogic = runUnification . down . sols . unLogic
-
--- >>> runLogic $ memb key dict
--- Right
---   ( [ pair (Con 1) (Con 2) , pair (Con 1) (Con 4) ]
---   , Substitution {}
---   )
--- >>> evalLogic $ memb key dict
--- [ pair (Con 1) (Con 2) , pair (Con 1) (Con 4) ]
--- >>> runLogic $ memb2 key dict
--- Left IncompatibleUnification
--- >>> evalLogic $ memb2 key dict
--- []
-
--- The substitution contains the last used substitution:
-
--- >>> runLogic $ memb2 (pair (Var 1) (Var 2)) dict
--- Left IncompatibleUnification
-
---------------------------------------------------------------------------------
--- Logic2, changing the order of the monadic stack
---------------------------------------------------------------------------------
-
--- this is morally s -> m [Either e (a, s)]
-newtype Logic2 s e m a = Logic2 { unLogic2 :: StateT s (ExceptT e (BacktrT m)) a }
-  deriving (Functor, Applicative, Monad)
-  deriving Backtr via StateT s (ExceptT e (BacktrT m))
-
-runLogic2 :: (Monad m) => Logic2 s e m a -> s -> m [Either e (a, s)]
-runLogic2 l ini = down . sols . runExceptT . ($ini) . runStateT $ unLogic2 l
-
-evalLogic2 :: (Monad m) => Logic2 s e m a -> s -> m [(a, s)]
-evalLogic2 l ini = fmap rights . down . sols . runExceptT . ($ini) . runStateT $ unLogic2 l
-
-evalLogic2' :: (Monad m) => Logic2 s e m a -> s -> m [a]
-evalLogic2' l ini = fmap (Prelude.map fst . rights) . down . sols . runExceptT . ($ini) . runStateT $ unLogic2 l
-
-instance Backtr m => Backtr (ExceptT e m) where
-  fail = ExceptT fail
-  (ExceptT t1) `amb` (ExceptT t2) = ExceptT $ t1 `amb` t2
-  once (ExceptT t) = ExceptT (fmap (Right . listToMaybe . rights) $ sols t)
-  sols (ExceptT t) = ExceptT (fmap sequence $ sols t)
-
-instance Cut m => Cut (ExceptT e m) where
-  cut  = ExceptT $ fmap Right cut
-  call (ExceptT t) = ExceptT (call t)
-
-instance Backtr m => Backtr (StateT s m) where
-  fail = StateT (\_ -> fail)
-  (StateT a) `amb` (StateT b) = StateT $ \s -> a s `amb` b s
-  once (StateT a) = StateT $ \s -> fmap ((,s) . fmap fst) . once $ a s
-  sols (StateT t) = StateT $ \s -> fmap ((,s) . fmap fst) . sols $ t s
-
-instance Cut m => Cut (StateT s m) where
-  cut = StateT $ \s -> fmap (,s) cut
-  call (StateT t) = StateT $ \s -> call (t s)
-
---------------------------------------------------------------------------------
--- Logic3, directly using UnificationT
---------------------------------------------------------------------------------
-
--- this is morally s -> m [Either e (a, s)]
-newtype Logic3 a = Logic3 { unLogic3 :: UnificationT (CutT Identity) a }
-  deriving (Functor, Applicative, Monad, Backtr, Cut, MonadState Substitution, MonadError UnificationError)
-    via StateT Substitution (ExceptT UnificationError (CutT Identity))
-
-runLogic3 :: Logic3 a -> s -> [Either UnificationError (a, Substitution)]
-runLogic3 l ini = runIdentity . down . sols . runUnificationT $ unLogic3 l
-
-evalLogic3 :: Logic3 a -> s -> [(a, Substitution)]
-evalLogic3 l ini = runIdentity . fmap rights . down . sols . runUnificationT $ unLogic3 l
-
-evalLogic3' :: Logic3 a -> s -> [a]
-evalLogic3' l ini = runIdentity . fmap (Prelude.map fst . rights) . down . sols . runUnificationT $ unLogic3 l
-
-(===) :: Unifiable a => a -> a -> Logic3 a
-a === b = Logic3 (unifyVal a b)
-
-memb :: (Unifiable (Term a)) => Term a -> [Term a] -> Logic3 (Term a)
-memb a []     = fail
+memb :: (Unifiable (Term a)) => Term a -> [Term a] -> Logic (Term a)
+memb _ []     = flop
 memb a (b:bs) = a === b `amb` memb a bs
 
--- >>> flip runLogic3 Subst.empty $ memb key dict
--- [ Right (pair (Con 1) (Con 2), Substitution { Int -> [(1,Con 2)] })
+-- >>> runLogic $ memb key dict
+-- [ Right (pair (Con 1) (Con 2),Substitution { Int -> [(1,Con 2)] })
 -- , Left IncompatibleUnification
--- , Right (pair (Con 1) (Con 4), Substitution { Int -> [(1,Con 4)] })]
+-- , Right (pair (Con 1) (Con 4),Substitution { Int -> [(1,Con 4)] })]
 
--- >>> flip evalLogic3 Subst.empty $ memb key dict
--- [ (pair (Con 1) (Con 2),Substitution { Int -> [(1,Con 2)] })
--- , (pair (Con 1) (Con 4),Substitution { Int -> [(1,Con 4)] })]
-
--- >>> flip evalLogic3' Subst.empty $ memb key dict
+-- >>> evalLogic $ memb key dict
 -- [ pair (Con 1) (Con 2) , pair (Con 1) (Con 4) ]
 
--- >>> flip runLogic3 Subst.empty $ memb (pair (Var 1) (Var 2)) dict
--- [ Right (pair (Con 1) (Con 2), Substitution { Int -> [(1,Con 1),(2,Con 2)] })
--- , Right (pair (Con 2) (Con 3), Substitution { Int -> [(1,Con 2),(2,Con 3)] })
--- , Right (pair (Con 1) (Con 4), Substitution { Int -> [(1,Con 1),(2,Con 4)] })]
+-- >>> runLogic $ memb (pair (Var 1) (Var 2)) dict
+-- [ Right (pair (Con 1) (Con 2),Substitution { Int -> [(1,Con 1),(2,Con 2)] })
+-- , Right (pair (Con 2) (Con 3),Substitution { Int -> [(1,Con 2),(2,Con 3)] })
+-- , Right (pair (Con 1) (Con 4),Substitution { Int -> [(1,Con 1),(2,Con 4)] })]
+
+--------------------------------------------------------------------------------
+-- Example from the paper
+--------------------------------------------------------------------------------
+
+child :: String -> Logic String
+child "terach"  = pure "abraham" `amb` pure "nachor" `amb` pure "haran"
+child "abraham" = pure "isaac"
+child "haran"   = pure "lot" `amb` pure "milcah" `amb` pure "yiscah"
+child "sarah"   = pure "isaac"
+child _         = flop
+
+grandChild :: String -> Logic String
+grandChild = child >=> child
+
+-- >>> evalLogic $ child "terach"
+-- [ "abraham" , "nachor" , "haran" ]
+
+-- >>> evalLogic $ grandChild "terach"
+-- [ "isaac" , "lot" , "milcah" , "yiscah" ]
+
+-- >>> take 2 . evalLogic $ grandChild "terach"
+-- [ "isaac" , "lot" ]
+
+-- >>> evalLogic $ child "isaac"
+-- []
+
+-- >>> evalLogic $ once (child "isaac")
+-- [ Nothing ]
+
+--------------------------------------------------------------------------------
+-- Appendix, lifted values for convenience
+--------------------------------------------------------------------------------
+
+true :: Monad m => m ()
+true = pure ()
+
+just :: (Monad m) => a -> m (Maybe a) -> m (Maybe a)
+just a _ = pure (Just a)
+
+nothing :: (Monad m) => m (Maybe a)
+nothing = pure Nothing
+
+nil :: (Monad m) => m [a]
+nil = pure []
+
+cons :: (Monad m) => a -> m [a] -> m [a]
+cons a ms = do
+  as <- ms
+  pure (a : as)
+
+answer :: (Monad m) => m b -> (a -> m b -> m b) -> Answer m a -> m b
+answer n _ No = n
+answer n y (Yes a m) = y a (m >>= answer n y)
+
+yes :: (Monad m) => a -> m (Answer m a) -> m (Answer m a)
+yes a mA = pure (Yes a mA)
+
+no :: (Monad m) => m (Answer m a)
+no = pure No
